db311ba93dfc271587848ea763cf202040cc69bf
JoyWambui <joywambuindegwa@gmail.com>
2021-12-12 15:47:11 +0300

    Create sign up view

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
+from django.contrib import messages
 
-# Create your views here.
+
+def signup(request):
+    '''View function that signs up a new user'''
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            messages.success(request, "Congratulations! Your user account has been created.")
+    else:
+        form = SignUpForm()
+        
+    title = 'Create New Account'
+    context={
+        'title': title,
+        'form': form,
+        }
+    return render(request, 'registration/signup.html', context)
